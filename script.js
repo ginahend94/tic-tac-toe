91e3650c4d71e551d5ed9c06783b83d5407c32a6
Gina Henderson <gina@ginahenderson.me>
2022-04-07 23:42:53 -0500

    Build out game functionality

---
+++ script.js
@@ -0,0 +1,111 @@
+// OBJECTS
+// Gameboard
+const Gameboard = (() => {
+    // let board = [
+    //     'x','x','o',
+    //     'o','x','x',
+    //     'x','o','o',
+    // ];
+
+    let board = new Array(9);
+
+    let xPlaying = true;
+
+    const playTurn = (slot, marker) => {
+        const turnLabel = document.getElementById('turnLabel');
+        switchTurn();
+        turnLabel.textContent = `${xPlaying ? 'x' : 'o'}'s turn`;
+        board[slot] = marker;
+        if (winState() && winState() != 'draw') {
+            turnLabel.textContent = `${marker} wins!`
+        }
+        if (winState() == 'draw') {
+            turnLabel.textContent = `It's a draw!`;
+        }
+        showBoard();
+    }
+
+    const winState = () => {
+        // Create array of winning indices
+        const winners = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+
+        // go through board
+        // for each index, if all have same marker, return true
+        let isWin = winners.some(winner => {
+            if (!board[winner[0]] || !board[winner[1]] || !board[winner[2]]) return false;
+            console.log(board[winner[0]]);
+            console.log(board[winner[1]]);
+            console.log(board[winner[1]]);
+            console.log(board[winner[0]] == board[winner[1]]);
+            console.log(board[winner[1]] == board[winner[2]]);
+            return board[winner[0]] == board[winner[1]] && board[winner[1]] == board[winner[2]]
+        })
+        if (isWin) return isWin;
+        console.log(board.every(a => {
+            console.log(!!a);
+            console.log(board.indexOf(a));
+        }))
+        if (board.every(a => a)) return 'draw';
+    }
+
+    const switchTurn = () => xPlaying = !xPlaying;
+
+    const showBoard = () => {
+        const main = document.querySelector('.gameboard');
+        main.innerHTML = '';
+        for (let i = 0; i < board.length; i++) {
+            const slot = document.createElement('div');
+            main.appendChild(slot);
+            slot.classList.add('slot');
+            slot.dataset.id = i;
+            slot.dataset.marker = board[i];
+            if (!board[i]) {
+                slot.addEventListener(
+                    'click', 
+                    playTurn.bind(slot, i, xPlaying ? 'x' : 'o', 
+                    {once:true})
+                );
+                slot.style.cursor = 'pointer';
+            }
+        }
+    }
+
+    return {showBoard};
+})();
+
+Gameboard.showBoard();
+
+// Game flow
+const GameFlow = (() => {
+})();
+
+const Player = (marker) => {
+    this.marker = marker;
+    return {marker, who};
+}
+// Player 1
+
+// Player 2
+// Display
+
+
+// User selects X or O
+// Computer is set to opposite of what player chooses
+// gameboard is displayed on screen
+// when player clicks a square, square index is logged in player moves
+// computer searches for empty slot and places marker
+// comp move is logged
+// each move is logged in gameboard
+// game searches gameboard for win
+// if win condition is met, game ends
+// Winner is displayed ib screen
+// Option to play again is displayed on screen
